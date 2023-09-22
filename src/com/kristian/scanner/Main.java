bf55120784bb9805f91fed0cfd53b5fb3bf38123
Kbidros <kristianbidros@gmail.com>
2023-09-22 13:47:18 +0200

    scanner practice

--- src/com/kristian/scanner/Main.java
+++ src/com/kristian/scanner/Main.java
@@ -1,15 +1,47 @@
 package com.kristian.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        boolean isPlaying = true;
 
-        System.out.println("App is running");
+        do {
+
+            int result = scannerNumber();
+
+            if (result > 1000){
+                isPlaying = false;
+                break;
+
+            }
+
+            for (int i = 0; i < result; i++) {
+                System.out.println(i);
+                System.out.println("Looping...");
+            }
 
 
+
+
+        } while (isPlaying);
 
     }
 
+    public static int scannerNumber(){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Please insert a number");
+        return sc.nextInt(); //radix?
+    }
+    public static String scannerText () {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        String input = sc.nextLine();
+
+        return input;
+    }
 
 }
