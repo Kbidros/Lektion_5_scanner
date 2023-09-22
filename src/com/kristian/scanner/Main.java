97ad95164028880c10d7985e70ca92d7a651c74f
Kbidros <kristianbidros@gmail.com>
2023-09-22 14:35:50 +0200

    basic scanner example

--- src/com/kristian/scanner/Main.java
+++ src/com/kristian/scanner/Main.java
@@ -5,43 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        boolean isPlaying = true;
+            Scanner sc = new Scanner(System.in);
 
-        do {
+            do {
 
-            int result = scannerNumber();
+                System.out.println("Input Number!");
+                int result = sc.nextInt();
 
-            if (result > 1000){
-                isPlaying = false;
-                break;
+                System.out.println("You picked: " + result);
 
-            }
+            } while(true);
 
-            for (int i = 0; i < result; i++) {
-                System.out.println(i);
-                System.out.println("Looping...");
-            }
-
-
-
-
-        } while (isPlaying);
-
-    }
-
-    public static int scannerNumber(){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Please insert a number");
-        return sc.nextInt(); //radix?
-    }
-    public static String scannerText () {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Insert some sentence");
-        String input = sc.nextLine();
-
-        return input;
     }
 
 }
